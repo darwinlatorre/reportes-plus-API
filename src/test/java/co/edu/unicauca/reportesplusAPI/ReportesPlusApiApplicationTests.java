fddcc5a5ba8293148ef40a24f82d632e6d23fbd6
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2024-03-08 15:14:58 -0500

    Project init

---
+++ src/test/java/co/edu/unicauca/reportesplusAPI/ReportesPlusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.reportesplusAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportesPlusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
