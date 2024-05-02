f09b1df636e00e9aa87a46f6b33835c71a232ab0
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2024-05-01 23:22:36 -0500

    Refactor GastosDAO class to remove unused method

--- src/main/java/co/edu/unicauca/reportesplusAPI/reportePosgrados/gastos/DAO/GastosDAO.java
+++ src/main/java/co/edu/unicauca/reportesplusAPI/reportePosgrados/gastos/DAO/GastosDAO.java
@@ -63,34 +63,4 @@ public class GastosDAO {
 		}
 		return GastoEntityList;
 	}
-
-	public List<GastoEntity> encontrarTodosReporteGastos() throws SQLException {
-
-		String consultaSQL = "SELECT m.ID, m.MOCDCDTD, m.MOCDCDND, m.MOCDFECH, m.MOCDCUMO, m.MOCDOBSE, m.MOCDVADE,m.MOCDVADE, m.MOCDVADE, m.MOCDVADE, m.MOCDVADE, m.MOCDESTA FROM MOCD m";
-
-		List<GastoEntity> reportes = new ArrayList<>();
-
-		try (PreparedStatement stmt = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection()
-				.prepareStatement(consultaSQL);
-				ResultSet rs = stmt.executeQuery()) {
-			while (rs.next()) {
-				GastoEntity reporte = new GastoEntity();
-				reporte.setID((rs.getInt("ID")));
-				reporte.setTipo_documento(rs.getString("MOCDCDTD"));
-				reporte.setNumero_movimiento(rs.getInt("MOCDCDND"));
-				reporte.setFecha(rs.getDate("MOCDFECH"));
-				reporte.setCuenta_movimiento(rs.getString("MOCDCUMO"));
-				reporte.setObservacion(rs.getString("MOCDOBSE"));
-				reporte.setValor_definitivo(rs.getBigDecimal("MOCDVADE"));
-				reporte.setValor_registro(rs.getBigDecimal("MOCDVADE"));
-				reporte.setValor_ejecutado(rs.getBigDecimal("MOCDVADE"));
-				reporte.setValor_pagado(rs.getBigDecimal("MOCDVADE"));
-				reporte.setSaldo(rs.getBigDecimal("MOCDVADE"));
-				reporte.setEstado(rs.getString("MOCDESTA"));
-				reportes.add(reporte);
-			}
-		}
-		return reportes;
-	}
-
 }
