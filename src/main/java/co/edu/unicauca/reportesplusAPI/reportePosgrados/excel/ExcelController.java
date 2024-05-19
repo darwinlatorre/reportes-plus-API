8e9f13fcaa05c655fcc75856074c5c8f777be7a4
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2024-05-18 23:13:02 -0500

    Add: Request map for excel controller

--- src/main/java/co/edu/unicauca/reportesplusAPI/reportePosgrados/excel/ExcelController.java
+++ src/main/java/co/edu/unicauca/reportesplusAPI/reportePosgrados/excel/ExcelController.java
@@ -1,8 +1,10 @@
 package co.edu.unicauca.reportesplusAPI.reportePosgrados.excel;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/posgrados/reporte/excel")
 public class ExcelController {
 
 }
